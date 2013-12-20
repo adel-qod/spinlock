1e3f717a0eddbe9a6194e0411330532cf00fb243
Adel Qodmani <mpcadel@gmail.com>
2013-12-20 13:44:07 +0200

    added more testing to unlock

--- test_suit.cc
+++ test_suit.cc
@@ -20,9 +20,18 @@ void count()
 
 void lock_test()
 {
-	//spin.lock();
+	spin.lock();
 	std::cout<<"Hello!\n";
 	// Since I won't lock it, only one thread should print the msg
+}
+
+void unlock_test()
+{
+	// This tests whether a thread can unlock a spinlock it doesn't
+	// own. We'll need three threads; one to acquire the spinlock, one to 
+	// actually block on it and a third one to try and release it
+	spin.lock();
+	std::cout<<"You should see this only once\n";
 }
 
 int main()
@@ -33,9 +42,18 @@ int main()
 	for(int i = 0; i < T_COUNT; i++)
 		t[i].join();
 	std::cout<<counter<<std::endl;
-	std::thread t1(lock_test);
-	std::thread t2(lock_test);
-	t1.join();
-	t2.join();
+
+	// std::thread t1(lock_test);
+	// std::thread t2(lock_test);
+	// t1.join();
+	// t2.join();
+
+	std::thread t3(unlock_test);
+	std::cout<<spin.unlock();
+	std::thread t4(unlock_test);
+	std::cout<<spin.unlock();
+	t3.join();
+	t4.join();
+
 	return 0;
 }
